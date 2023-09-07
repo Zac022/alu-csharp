a60895be286a8ec2198f48b3ef9b40b91d16b46f
Zac022 <i.rudasingwa@alustudent.com>
2023-09-07 13:57:16 +0000

    12. Hello, Error

---
+++ csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Error.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
+        Environment.Exit(1);
+    }
+}
