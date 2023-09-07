250ba03ed7e24f6cbef0815489ec235d910c773d
Zac022 <i.rudasingwa@alustudent.com>
2023-09-07 11:00:51 +0000

    6. Print string

---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str = "Holberton School";
+        Console.Write(str + str + str + "\n" + str.Substring(0, 9) + "\n");
+    }
+}
