6a60d643e35e990fa5592cc456b081a827b512a3
Zac022 <i.rudasingwa@alustudent.com>
2023-09-07 09:33:36 +0000

    init

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Check if the 'dotnet' command is available
+if ! command -v dotnet &> /dev/null
+then
+    echo "Error: 'dotnet' command not found. Please install the .NET SDK."
+    exit 1
+fi
+
 # Create a new directory for the project
 mkdir 0-new_project
 
