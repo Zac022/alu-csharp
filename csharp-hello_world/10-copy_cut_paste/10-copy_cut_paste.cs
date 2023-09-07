b141849d64d8716debe54855214aeb7c28f6ded3
Zac022 <i.rudasingwa@alustudent.com>
2023-09-07 13:38:58 +0000

    init

--- csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
+++ csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
@@ -2,15 +2,14 @@ using System;
 
 class Program
 {
-    static void Main()
-    {
-        string word = "Holberton";
-        string word_first_3 = word.Substring(0, 3);
-        string word_last_2 = word.Substring(word.Length - 2);
-        string middle_word = word.Substring(1, word.Length - 2);
-
-        Console.WriteLine($"First 3 letters: {word_first_3}");
-        Console.WriteLine($"Last 2 letters: {word_last_2}");
-        Console.WriteLine($"Middle word: {middle_word}");
-    }
+        static void Main(string[] args)
+        {
+		string word = "Holberton";
+		string word_three = word.Substring(0,3);
+		string word_two =  word.Substring(7,2);
+		string word_middle = word.Substring(1, 7);
+		Console.WriteLine("First 3 letters: {0}", word_three);
+		Console.WriteLine("Last 2 letters: {0}", word_two);
+		Console.WriteLine("Middle word: {0}", word_middle);
+        }
 }
