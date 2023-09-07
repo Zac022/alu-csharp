a0a9f605b137f65b94379cd48b1b4f5ae1ac022e
Zac022 <i.rudasingwa@alustudent.com>
2023-09-07 10:51:39 +0000

    init

--- csharp-hello_world/5-print_float/5-print_float.cs
+++ csharp-hello_world/5-print_float/5-print_float.cs
@@ -4,8 +4,8 @@ class Program
 {
     static void Main()
     {
-        float number = 3.14159265f;
-        Console.WriteLine($"Float: {number:f2}");
+        int number = 972;
+        Console.WriteLine(number + " Mission Street");
     }
 }
 
