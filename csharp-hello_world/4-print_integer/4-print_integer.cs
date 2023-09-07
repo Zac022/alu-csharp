4bc681041f5554819ffbac41a3193a7b54f9ddc1
Zac022 <i.rudasingwa@alustudent.com>
2023-09-07 10:22:03 +0000

    4. Print integer

---
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 972; // Replace with your desired integer value
+        string street = "Mission Street";
+
+        // Print the number, followed by " Mission Street" and a new line
+        Console.WriteLine(number + " " + street);
+    }
+}
+
