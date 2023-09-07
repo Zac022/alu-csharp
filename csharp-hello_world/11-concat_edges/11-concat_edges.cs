514079b56b4f3692a9fd8a7d8feeb5c966e6dbfe
Zac022 <i.rudasingwa@alustudent.com>
2023-09-07 13:47:10 +0000

    init

--- csharp-hello_world/11-concat_edges/11-concat_edges.cs
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -5,11 +5,9 @@ class Program
     static void Main()
     {
         string s1 = "object";
-        string s2 = "programming";
-        string s3 = "oriented";
-        string s4 = " in ";
-        string s5 = "C#";
-
-        Console.WriteLine(s1 + "-" + s2 + "-" + s3 + s4 + s5);
+        string s2 = "-oriented";
+        string s3 = " programming";
+        string s4 = " in C#";
+        Console.WriteLine(s1 + s2 + s3 + s4);
     }
 }
