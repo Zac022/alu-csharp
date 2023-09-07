5f50bb1eedc02bca6b0c86e662fe462833055c85
Zac022 <i.rudasingwa@alustudent.com>
2023-09-07 14:01:37 +0000

    0-initialize_new_project.sh

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,21 +1,3 @@
 #!/usr/bin/env bash
 
-# Check if the 'dotnet' command is available
-if ! command -v dotnet &> /dev/null
-then
-    echo "Error: 'dotnet' command not found. Please install the .NET SDK."
-    exit 1
-fi
-
-# Create a new directory for the project
-mkdir 0-new_project
-
-# Change the working directory to the newly created project folder
-cd 0-new_project
-
-# Initialize a new C# console application project using dotnet
-dotnet new console
-
-# Display a success message
-echo "The template 'Console Application' was created successfully."
-
+dotnet new console -n 0-new_project
