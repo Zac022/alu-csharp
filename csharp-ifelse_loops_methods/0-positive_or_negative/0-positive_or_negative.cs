d00823b218b36583dd0279385f3995d8a027b854
Zac022 <i.rudasingwa@alustudent.com>
2023-09-08 10:16:12 +0000

    init

--- csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -4,22 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random random = new Random();
-        int number = random.Next(-10, 11); // Generate a random number between -10 and 10
-
-        Console.Write(number + " is ");
-
-        if (number > 0)
-        {
-            Console.WriteLine("positive");
-        }
-        else if (number < 0)
-        {
-            Console.WriteLine("negative");
-        }
-        else
-        {
-            Console.WriteLine("zero");
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+        if(number > 0){
+            Console.WriteLine($"{number} is positive");
+        }else if(number < 0){
+            Console.WriteLine($"{number} is negative");
+        }else{
+            Console.WriteLine($"{number} is zero");
         }
     }
 }
