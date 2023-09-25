8f9123660bb607f368407cc4b0bfa995ba0bd242
Isaac <rudasaacv@gmail.com>
2023-09-25 09:57:07 +0200

    init

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,29 +1,47 @@
-using System.Collections;
-class Array{
+using System;
 
-    public static int[] CreatePrint(int size){
-
-        if(size == 0){
-            Console.WriteLine();
-            return new int[0];
-        }else if(size < 0){
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
             Console.WriteLine("Size cannot be negative");
-            return null;
+            return new int[0]; // Return an empty array
+        }
+        else if (size == 0)
+        {
+            Console.WriteLine();
+            return new int[0]; // Return an empty array
         }
 
-        int[] NumberList = new int[size];
-        for(int i = 0; i < size; i ++){
-            NumberList[i] = i;
-            if(i < size - 1){
-                Console.Write($"{i} ");
-            }else{
-                Console.Write($"{i}");
-            }
-         
-
+        int[] newArray = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            newArray[i] = i;
+            Console.Write(i + " ");
         }
         Console.WriteLine();
 
-        return NumberList;
+        return newArray;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] newArray;
+
+        newArray = Array.CreatePrint(10);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = Array.CreatePrint(16);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = Array.CreatePrint(0);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = Array.CreatePrint(-10);
     }
 }
