f6daaaef8f785e2e8a71d3d24d4486774c78bf90
Zac022 <i.rudasingwa@alustudent.com>
2023-09-25 10:20:42 +0200

    init

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,47 +1,49 @@
 using System;
 
-class Array
+namespace MyNamespace // Optionally, use a namespace to avoid global namespace conflicts
 {
-    public static int[] CreatePrint(int size)
+    class Array
     {
-        if (size < 0)
+        public static int[] CreatePrint(int size)
         {
-            Console.WriteLine("Size cannot be negative");
-            return new int[0]; // Return an empty array
-        }
-        else if (size == 0)
-        {
+            if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return null;
+            }
+            else if (size == 0)
+            {
+                Console.WriteLine();
+                return new int[0];
+            }
+
+            int[] NumberList = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                NumberList[i] = i;
+                if (i < size - 1)
+                {
+                    Console.Write($"{i} ");
+                }
+                else
+                {
+                    Console.Write($"{i}");
+                }
+            }
             Console.WriteLine();
-            return new int[0]; // Return an empty array
-        }
 
-        int[] newArray = new int[size];
-        for (int i = 0; i < size; i++)
-        {
-            newArray[i] = i;
-            Console.Write(i + " ");
+            return NumberList;
         }
-        Console.WriteLine();
-
-        return newArray;
     }
-}
 
-class Program
-{
-    static void Main(string[] args)
+    class Program
     {
-        int[] newArray;
+        static void Main(string[] args)
+        {
+            int[] newArray;
 
-        newArray = Array.CreatePrint(10);
-        Console.WriteLine("Array Length: " + newArray.Length);
-        Console.WriteLine("----------------");
-        newArray = Array.CreatePrint(16);
-        Console.WriteLine("Array Length: " + newArray.Length);
-        Console.WriteLine("----------------");
-        newArray = Array.CreatePrint(0);
-        Console.WriteLine("Array Length: " + newArray.Length);
-        Console.WriteLine("----------------");
-        newArray = Array.CreatePrint(-10);
+            newArray = Array.CreatePrint(5);
+            Console.WriteLine("Array Length: " + newArray.Length);
+        }
     }
 }
