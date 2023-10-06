434308f2191baebf8c8717e8d6b5feb0e39b1ac3
Zac022 <i.rudasingwa@alustudent.com>
2023-10-06 09:59:50 +0200

    init

---
+++ csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> newNode = myLList.AddFirst(n);
+        return newNode;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedList<int> myLList = new LinkedList<int>();
+
+        myLList.AddLast(1);
+        myLList.AddLast(2);
+        myLList.AddLast(3);
+        myLList.AddLast(4);
+        myLList.AddLast(5);
+
+        foreach (int i in myLList)
+            Console.Write(i + " ");
+        Console.WriteLine();
+
+        LList.Add(myLList, 0);
+
+        foreach (int i in myLList)
+            Console.Write(i + " ");
+        Console.WriteLine();
+    }
+}
