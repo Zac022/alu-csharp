fb02afc42863ac22d2379a0a3c7a53815447e54d
Zac022 <i.rudasingwa@alustudent.com>
2023-10-14 10:51:32 +0200

    init

--- csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -1,20 +1,11 @@
-using System;
 using System.Collections.Generic;
 
-class LList
-{
-    public static int Pop(LinkedList<int> myLList)
-    {
-        if (myLList.Count == 0)
-        {
-            return 0; // List is empty, return 0
+class LList{
+    public static int Pop(LinkedList<int> myList){
+        if(myList.Count == 0){
+            return 0;
+        }else{
+            return myList.First.Value;
         }
-
-        int headValue = myLList.First.Value;
-        myLList.RemoveFirst();
-
-        return headValue;
     }
 }
-
-
