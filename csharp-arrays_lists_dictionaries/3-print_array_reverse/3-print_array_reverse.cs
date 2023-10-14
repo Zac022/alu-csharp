42a8efdc159be83049e6cd523918ae502e0aac08
Zac022 <i.rudasingwa@alustudent.com>
2023-10-14 10:12:54 +0200

    init

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -1,20 +1,24 @@
-using System;
+using System.Collections;
 
-class Array
-{
-    public static void Reverse(int[] array)
-    {
-        if (array == null)
-        {
-            Console.WriteLine("Array is null");
-            return;
-        }
+class Array{
 
-        for (int i = array.Length - 1; i >= 0; i--)
-        {
-            Console.Write(array[i] + " ");
-        }
+    public static void Reverse(int[] array){
+     
+       if(array != null){
+            int size = array.Length;
+        if(size > 0 && size != null){
+
+            for(int i = size - 1; i >= 0; i--){
+                Console.Write(array[i] + " ");
+                }
+                    Console.WriteLine();
+             }else{
+                Console.WriteLine();
+             }
+       }else{
         Console.WriteLine();
+       }
+       
+     
     }
 }
-
