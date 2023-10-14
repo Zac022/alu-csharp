1db677948ae9dac59daae4c97d4cf5a3e40fbf6a
Zac022 <i.rudasingwa@alustudent.com>
2023-10-14 09:10:34 +0200

    init

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -26,22 +26,3 @@ class Array
         return newArray;
     }
 }
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        int[] newArray;
-
-        newArray = Array.CreatePrint(10);
-        Console.WriteLine("Array Length: " + newArray.Length);
-        Console.WriteLine("----------------");
-        newArray = Array.CreatePrint(16);
-        Console.WriteLine("Array Length: " + newArray.Length);
-        Console.WriteLine("----------------");
-        newArray = Array.CreatePrint(0);
-        Console.WriteLine("Array Length: " + newArray.Length);
-        Console.WriteLine("----------------");
-        newArray = Array.CreatePrint(-10);
-    }
-}
