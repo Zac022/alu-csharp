b520a64eb6f4cebb8be0513dcc508b0cce20a87b
Zac022 <i.rudasingwa@alustudent.com>
2023-10-16 12:12:26 +0200

    changes

--- csharp-exceptions/1-divide_print/1-divide_print.cs
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -1,16 +1,23 @@
-using System.Collections;
+using System;
 
-class Int{
-
-    public static void divide(int a, int b){
-        int result = 0;
-
-        try{
-            result = a / b;
-        }catch(System.Exception){
-            Console.WriteLine("Cannot divide by zero");       
-        }finally{
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        try
+        {
+            int result = a / b;
             Console.WriteLine($"{a} / {b} = {result}");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine($"{a} / {b} = {(b == 0 ? 0 : a / b)}");
         }
     }
 }
+
+
