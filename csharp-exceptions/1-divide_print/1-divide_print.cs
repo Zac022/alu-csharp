80c93ebd278ce7016de4a2fde468bf36fbf4b9e3
Zac022 <i.rudasingwa@alustudent.com>
2023-10-16 11:46:39 +0200

    init

--- csharp-exceptions/1-divide_print/1-divide_print.cs
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -20,20 +20,4 @@ class Int
     }
 }
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        int a, b;
-
-        a = 4;
-        b = 3;
-        Int.divide(a, b);
-        Console.WriteLine();
-
-        a = 9;
-        b = 0;
-        Int.divide(a, b);
-    }
-}
 
