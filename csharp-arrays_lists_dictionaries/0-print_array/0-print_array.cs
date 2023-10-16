4cd6b246969686ccb9cfb03cc73220e68ada5308
Zac022 <i.rudasingwa@alustudent.com>
2023-10-16 15:01:10 +0200

    init

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,28 +1,29 @@
-using System;
+using System.Collections;
+class Array{
 
-class Array
-{
-    public static int[] CreatePrint(int size)
-    {
-        if (size < 0)
-        {
-            Console.WriteLine("Size cannot be negative");
-            return new int[0]; // Return an empty array
-        }
-        else if (size == 0)
-        {
+    public static int[] CreatePrint(int size){
+
+        if(size == 0){
             Console.WriteLine();
-            return new int[0]; // Return an empty array
+            return new int[0];
+        }else if(size < 0){
+            Console.WriteLine("Size cannot be negative");
+            return null;
         }
 
-        int[] newArray = new int[size];
-        for (int i = 0; i < size; i++)
-        {
-            newArray[i] = i;
-            Console.Write(i + " ");
+        int[] NumberList = new int[size];
+        for(int i = 0; i < size; i ++){
+            NumberList[i] = i;
+            if(i < size - 1){
+                Console.Write($"{i} ");
+            }else{
+                Console.Write($"{i}");
+            }
+         
+
         }
         Console.WriteLine();
 
-        return newArray;
+        return NumberList;
     }
 }
