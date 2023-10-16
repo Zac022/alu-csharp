e4088e868a275b351be19799e472dd5c0baba93c
Zac022 <i.rudasingwa@alustudent.com>
2023-10-16 15:20:26 +0200

    init

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,40 +1,53 @@
-using System;
-using System.Collections.Generic;
+using System.Collections;
 
-class MyStack
-{
-    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+class MyStack{
+
+        static void Main(string[] args)
     {
+        Stack<string> aStack = new Stack<string>();
+
+        aStack.Push("C");
+        aStack.Push("HTML");
+        aStack.Push("Javascript");
+        aStack.Push("Python");
+        aStack.Push("React");
+        aStack.Push("Ruby");
+
+        foreach (string item in aStack)
+            Console.WriteLine(item);
+
+        Console.WriteLine("------");
+
+        MyStack.Info(aStack, "C#", "Javascript");
+
+        Console.WriteLine("------");
+
+        foreach (string item in aStack)
+            Console.WriteLine(item);
+    }
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search){
         Console.WriteLine($"Number of items: {aStack.Count}");
 
-        if (aStack.Count == 0)
-        {
-            Console.WriteLine("Top item: Stack is empty");
-            Console.WriteLine($"Stack contains \"{search}\": False");
-            aStack.Push(newItem);
-            return aStack;
+        if(aStack.Count == 0){
+            Console.WriteLine("Stack is empty");
+        }else{
+            Console.WriteLine($"Top item: {aStack.Peek()}");
         }
 
-        string topItem = aStack.Peek();
-        Console.WriteLine($"Top item: {topItem}");
+        if(aStack.Contains(search)){
+            Console.WriteLine($"Stack contains \"{search}\" : True");
+        }else{
+            Console.WriteLine($"Stack contains \"{search}\" : False");
+        }
 
-        bool containsSearch = aStack.Contains(search);
-        Console.WriteLine($"Stack contains \"{search}\": {containsSearch}");
-
-        if (containsSearch)
-        {
-            while (aStack.Count > 0)
-            {
-                string item = aStack.Pop();
-                if (item == search)
-                {
-                    break;
-                }
+        for(int i = 0; i <= aStack.Count; i++){
+            if(aStack.Contains(search)){
+                string temp = aStack.Pop();
             }
         }
 
         aStack.Push(newItem);
+
         return aStack;
     }
 }
-
