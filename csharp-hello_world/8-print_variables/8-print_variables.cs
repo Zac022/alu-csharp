662e5e9704831926697331a8e713ce914f8b0abe
Zac022 <i.rudasingwa@alustudent.com>
2023-10-16 14:26:13 +0200

    init

--- csharp-hello_world/8-print_variables/8-print_variables.cs
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        int number = 972;
-        string street = "Mission Street";
-
-        Console.WriteLine($"{number} {street}");
+        string str1 = "Street";
+        string str2 = "Mission";
+        int int1 = 972;
+        Console.WriteLine($"{int1} {str2} {str1}");
     }
 }
