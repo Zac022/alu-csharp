a2c730416e2d33313d2f78189de1e995f49d2f8a
Zac022 <i.rudasingwa@alustudent.com>
2023-10-16 12:07:10 +0200

    init

--- csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -1,41 +1,23 @@
+using System.Collections;
 using System;
-using System.Collections.Generic;
 
-class List
-{
-    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
-    {
-        List<int> result = new List<int>();
+class List{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength){
+        List<int> result  = new List<int>();
+        int TempHold = 0;
 
-        try
-        {
-            for (int i = 0; i < listLength; i++)
-            {
-                if (i >= list1.Count || i >= list2.Count)
-                {
-                    Console.WriteLine("Out of range");
-                    break;
-                }
-
-                if (list2[i] == 0)
-                {
-                    Console.WriteLine("Cannot divide by zero");
-                    result.Add(0);
-                }
-                else
-                {
-                    result.Add(list1[i] / list2[i]);
-                }
+        for(int i = 0; i < listLength ; i++){
+            try{
+                TempHold = list1[i] / list2[i];
+                result.Add(TempHold);
+            }catch(ArgumentOutOfRangeException){
+                Console.WriteLine("Out of range");
+            }catch(DivideByZeroException){
+                Console.WriteLine("Cannot divide by zero");
+                result.Add(0);
             }
-        }
-        catch (DivideByZeroException)
-        {
-            Console.WriteLine("Cannot divide by zero");
         }
 
         return result;
     }
 }
-
-
-
