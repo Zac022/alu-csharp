c6de9440142a6dc2b16cb4b86ad30707d2b9aec9
Zac022 <i.rudasingwa@alustudent.com>
2023-10-21 09:47:16 +0200

    0-add

---
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace  MyMath
+{
+    public class Operations
+    {
+
+        /// <summary>
+        /// Returns addition of two integers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int Add(int a , int b){
+            return a + b;
+        }
+    }
+}
