f08fd2de9b447ccc162179cbe440df065e9d8bd3
Zac022 <i.rudasingwa@alustudent.com>
2023-11-08 20:48:32 +0200

    change

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -1,36 +1,48 @@
 ﻿using System;
 
-class MatrixMath{
-    public static double[,] Rotate2D(double[,] matrix, double angle){
-        double[,] rotationMatrix = {
-            { Math.Cos(angle), -Math.Sin(angle)},
-            { Math.Sin(angle), Math.Cos(angle)}
-        };
-
+public class MatrixMath
+{
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
         int rows = matrix.GetLength(0);
         int cols = matrix.GetLength(1);
 
-        if(rows == cols && rows == 2 && cols == 2){
-         
-         double[,] result = new double[rows,cols];
-
-         for (int col = 0; col < cols; col++)
-            {
-                for (int row = 0; row < rows; row++)
-                {
-                    result[row, col] = 0;
-                    for (int k = 0; k < rows; k++)
-                    {
-                        result[row, col] += Math.Round(matrix[k, col] * rotationMatrix[row, k] , 2);
-                    }
-                }
-            }
-
-            return result;
+        // Check if the matrix is square (equal number of rows and columns)
+        if (rows != cols)
+        {
+            // Return a matrix containing -1 for invalid size
+            return new double[,] { { -1 } };
         }
 
-        return new double[,]{{-1}};
-       
+        // Create a new matrix to store the rotated values
+        double[,] rotatedMatrix = new double[rows, cols];
+
+        double cosTheta = Math.Cos(angle);
+        double sinTheta = Math.Sin(angle);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                // Apply rotation to each element in the matrix
+                double x = i - rows / 2.0;
+                double y = j - cols / 2.0;
+                double newX = x * cosTheta - y * sinTheta + rows / 2.0;
+                double newY = x * sinTheta + y * cosTheta + cols / 2.0;
+
+                // Check if the new coordinates are within the matrix bounds
+                if (newX >= 0 && newX < rows && newY >= 0 && newY < cols)
+                {
+                    rotatedMatrix[i, j] = matrix[(int)newX, (int)newY];
+                }
+                else
+                {
+                    // If the new coordinates are out of bounds, set the value to -1
+                    rotatedMatrix[i, j] = -1;
+                }
+            }
+        }
+
+        return rotatedMatrix;
     }
-  
 }
