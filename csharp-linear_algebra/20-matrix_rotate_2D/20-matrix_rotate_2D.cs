36f8b34095660ebb29379a577e39ba92243ea188
Zac022 <i.rudasingwa@alustudent.com>
2023-11-08 20:02:39 +0200

    delete

---
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class MatrixMath{
+    public static double[,] Rotate2D(double[,] matrix, double angle){
+        double[,] rotationMatrix = {
+            { Math.Cos(angle), -Math.Sin(angle)},
+            { Math.Sin(angle), Math.Cos(angle)}
+        };
+
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        if(rows == cols && rows == 2 && cols == 2){
+         
+         double[,] result = new double[rows,cols];
+
+         for (int col = 0; col < cols; col++)
+            {
+                for (int row = 0; row < rows; row++)
+                {
+                    result[row, col] = 0;
+                    for (int k = 0; k < rows; k++)
+                    {
+                        result[row, col] += Math.Round(matrix[k, col] * rotationMatrix[row, k] , 2);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        return new double[,]{{-1}};
+       
+    }
+  
+}
