961f41c2eccd4157ac349a5a0072c270bd4cd140
Zac022 <i.rudasingwa@alustudent.com>
2023-11-15 09:40:03 +0200

    init

--- csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-class Line{
-    public static void PrintDiagonal(int length){
-          if(length > 0){
-                for(int i = 0 ; i < length; i++){
-                Console.Write("\\");
-             }
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0)
+        {
+            Console.WriteLine();
         }
-        Console.WriteLine(); 
+        else
+        {
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" "); 
+                }
+                Console.WriteLine("\\");
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Line.PrintDiagonal(3);
+        Line.PrintDiagonal(0);
+        Line.PrintDiagonal(12);
+        Line.PrintDiagonal(-98);
     }
 }
